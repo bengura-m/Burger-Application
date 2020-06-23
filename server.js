380da6b9adba6121c6c2c2abc768a5381f87023b
Mitchelle <54994430+Mitchelle-B@users.noreply.github.com>
2020-06-23 21:26:43 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 3306;
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+// connection to the db 
+
+app.listen(PORT, function() {
+  console.log("Listening on port:%s", PORT);
+});
